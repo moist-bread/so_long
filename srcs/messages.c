c37fcbbded77c69de357954fbe5db4747f36df1a
moist-bread <raquelgo2work@gmail.com>
2025-02-19 10:35:01 +0000

    :3

--- srcs/messages.c
+++ srcs/messages.c
@@ -6,7 +6,7 @@
 /*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 12:15:04 by rduro-pe          #+#    #+#             */
-/*   Updated: 2025/02/10 16:09:40 by rduro-pe         ###   ########.fr       */
+/*   Updated: 2025/02/19 10:34:32 by rduro-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	main_menu(char *input)
 	ft_printf("\n\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗ \t\t\t\t\t\t ▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▚▛▘▜▝▞▟▙▘▖▞▙▗▚▛▘▜▝▞▟▛▙▘▖▞▙▗▞▙▗▚"
-		"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF "\n\n\t");
+				"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF "\n\n\t");
 	ft_printf(YEL "HIT " CYN "__ENTER__" YEL " TO START THE GAME ..." DEF);
 	line = get_next_line(1);
 	free(line);
@@ -32,12 +32,11 @@ void	main_menu_content(void)
 {
 	ft_printf(YEL "\n\n\tEAGERNESS ENTERTAINMENT PRESENTS ...\n\n" DEF);
 	ft_printf("\n" HCYN "▚▛▘▜▝▞▟▙▘▖▞▙▗▚▛▘▜▝▞▟▛▙▘▖▞▙▗▞▙▗▚"
-		"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF);
+				"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗ \t\t\t\t\t\t ▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗\t" DEF);
-	ft_printf(GRN "› OUR " YEL "BRAND NEW" GRN " HANDHELD: " BLK CYNB
-		" EAGN " DEF);
+	ft_printf(GRN "› OUR " YEL "BRAND NEW" GRN " HANDHELD: " BLK CYNB " EAGN " DEF);
 	ft_printf("\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗\t" DEF);
@@ -49,21 +48,21 @@ void	main_menu_content(void)
 	ft_printf("\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗\t" DEF);
-	ft_printf(BLK GRNB "› ~ game name ~ \t\t" DEF);
+	ft_printf(BLK GRNB "› COLLECTATRON 3000 \t\t" DEF);
 	ft_printf("\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗" DEF "\t\t\t\t\t\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▖▞▙▗ \t\t\t\t\t\t ▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▚▛▘▜▝▞▟▙▘▖▞▙▗▚▛▘▜▝▞▟▛▙▘▖▞▙▗▞▙▗▚"
-		"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF);
+				"▛▘▜▝▞▟▙▘▖▞▙▗▟▙▘▖▞▙▗▚▛▘" DEF);
 }
 
 void	main_menu_instruct(char *input)
 {
 	ft_printf("\n\n\n\n" HCYN "▚▗▛▘\t" DEF);
-	ft_printf(GRN "› you're a " CYN "flying robot" GRN " sent to " DEF);
+	ft_printf(GRN "› you're a " CYN "flying sd card reader" GRN "  " DEF);
 	ft_printf("\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n" HCYN "▚▛▘▗\t" DEF);
-	ft_printf(GRN "  collect valuable " YEL "SDCARDS\t" DEF);
+	ft_printf(GRN "  sent to collect valuable " YEL "SDCARDS" DEF);
 	ft_printf("\t " HCYN "▖▞▙▗" DEF);
 	ft_printf("\n\n\n" HCYN "▚▗▛▘\t" DEF);
 	ft_printf(GRN "› escape with them through the" YEL " VENT" DEF);
@@ -74,7 +73,7 @@ void	main_menu_instruct(char *input)
 	ft_printf("\n" HCYN "▚▛▘▗\t" DEF);
 	ft_printf(GRN "  or " CYN "__ARROW KEYS__ \t\t" DEF);
 	ft_printf("\t " HCYN "▖▞▙▗" DEF);
-	ft_printf(YEL"\n\n\t› input map: " CYN "%s" DEF, input);
+	ft_printf(YEL "\n\n\t› input map: " CYN "%s" DEF, input);
 }
 
 void	exit_message(int type)
